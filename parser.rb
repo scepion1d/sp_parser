58d7caeecb893dda92d7c907543271cf80c100fc
Prokhor Mikhailov <scepion1d.dev@gmail.com>
2022-08-07 10:32:04 +0200

    [fix] Added error handling into entry point

--- parser.rb
+++ parser.rb
@@ -5,7 +5,7 @@ Dir['./lib/**/*.rb', './app/**/*.rb'].each { |file| require file }
 
 file_path = ARGV[0]
 
-logs = FileProcessor.call(file_path)
-
-Stat::TotalVisits.new(logs).print(order: :dsc)
-Stat::UniqueVisits.new(logs).print(order: :dsc)
+if (logs = FileProcessor.call(file_path)).instance_of?(Array)
+  Stat::TotalVisits.new(logs).print(order: :dsc)
+  Stat::UniqueVisits.new(logs).print(order: :dsc)
+end
